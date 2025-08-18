14f0fe641e5ac88b3d7a753e68618b0be9c964f0
Mike <mikestewart729@gmail.com>
2025-08-18 17:09:19 -0400

    implement naive path-finding algorithm for CommonBeasts

--- src/beasts/common_beast.rs
+++ src/beasts/common_beast.rs
@@ -1,4 +1,6 @@
-use crate::{Coord, beasts::Beast, board::Board};
+use std::cmp::Ordering;
+
+use crate::{BOARD_WIDTH, BOARD_HEIGHT, Coord, Tile, beasts::Beast, board::Board};
 
 #[derive(Debug)]
 pub struct CommonBeast {
@@ -15,12 +17,225 @@ impl Beast for CommonBeast {
         board: &Board,
         player_position: &Coord,
     ) -> Option<Coord> {
-        let mut next_position = self.position;
-        if next_position.column > 0 {
-            next_position.column -= 1;
-            return Some(next_position);
-        }
+        // Top Row
+        let left_top = if self.position.column > 0 && self.position.row > 0 {
+            Some(Coord {
+                column: self.position.column - 1,
+                row: self.position.row - 1,
+            })
+        } else {
+            None
+        };
+        let middle_top = if self.position.row > 0 {
+            Some(Coord {
+                column: self.position.column,
+                row: self.position.row - 1,
+            })
+        } else {
+            None
+        };
+        let right_top = if self.position.column < BOARD_WIDTH - 1 && self.position.row > 0 {
+            Some(Coord {
+                column: self.position.column + 1,
+                row: self.position.row - 1,
+            })
+        } else { 
+            None
+        };
 
-        None
+        // Middle Row
+        let left_middle = if self.position.column > 0 {
+            Some(Coord {
+                column: self.position.column - 1,
+                row: self.position.row,
+            })
+        } else {
+            None
+        };
+        let right_middle = if self.position.column < BOARD_WIDTH - 1 {
+            Some(Coord {
+                column: self.position.column + 1,
+                row: self.position.row,
+            })
+        } else {
+            None
+        };
+
+        // Bottom Row
+        let left_bottom = 
+            if self.position.column > 0 && self.position.row < BOARD_HEIGHT - 1 
+        {
+            Some(Coord {
+                column: self.position.column - 1,
+                row: self.position.row + 1,
+            })
+        } else {
+            None
+        };
+        let middle_bottom = 
+            if self.position.row < BOARD_HEIGHT - 1
+        {
+            Some(Coord {
+                column: self.position.column,
+                row: self.position.row + 1,
+            })
+        } else {
+            None
+        };
+        let right_bottom = 
+            if self.position.column < BOARD_WIDTH - 1 && self.position.row < BOARD_HEIGHT - 1
+        {
+            Some(Coord {
+                column: self.position.column + 1,
+                row: self.position.row + 1,
+            })
+        } else {
+            None
+        };
+
+        let possible_moves = match (
+            player_position.column.cmp(&self.position.column),
+            player_position.row.cmp(&self.position.row),
+        ) {
+            (Ordering::Greater, Ordering::Greater) => {
+                /* player: right-bottom */
+				// 8 7  5
+				// 6 ├┤ 3
+				// 4 2  1
+                [
+                    right_bottom,
+                    middle_bottom,
+                    right_middle,
+                    left_bottom,
+                    right_top,
+                    left_middle,
+                    middle_top,
+                    left_top,
+                ]
+            },
+            (Ordering::Greater, Ordering::Less) => {
+                /* player: right-top */
+                // 4 2  1
+				// 6 ├┤ 3
+				// 8 7  5
+				[
+					right_top,
+					middle_top,
+					right_middle,
+					left_top,
+					right_bottom,
+					left_middle,
+					middle_bottom,
+					left_bottom,
+				]
+            },
+            (Ordering::Greater, Ordering::Equal) => {
+                /* player: right-middle */
+                // 6 4  2
+				// 8 ├┤ 1
+				// 7 5  3
+				[
+					right_middle,
+					right_top,
+					right_bottom,
+					middle_top,
+					middle_bottom,
+					left_top,
+					left_bottom,
+					left_middle,
+				]
+            },
+            (Ordering::Less, Ordering::Greater) => {
+                /* player: left-bottom */
+                // 4 6  8
+				// 2 ├┤ 7
+				// 1  3 5
+				[
+					left_bottom,
+					left_middle,
+					middle_bottom,
+					left_top,
+					right_bottom,
+					right_middle,
+					middle_top,
+					right_top,
+				]
+            },
+            (Ordering::Less, Ordering::Less) => {
+                /* player: left-top */
+                // 1  3 5
+				// 2 ├┤ 7
+				// 4 6  8
+				[
+					left_top,
+					left_middle,
+					middle_top,
+					left_bottom,
+					right_top,
+					middle_bottom,
+					right_middle,
+					right_bottom,
+				]
+            },
+            (Ordering::Less, Ordering::Equal) => {
+                /* player: left-middle */
+                // 2 4  6
+				// 1 ├┤ 8
+				// 3 5  7
+				[
+					left_middle,
+					left_top,
+					left_bottom,
+					middle_top,
+					middle_bottom,
+					right_top,
+					right_bottom,
+					right_middle,
+				]
+            },
+            (Ordering::Equal, Ordering::Greater) => {
+                /* player: middle-bottom */
+                // 6 8  7
+				// 4 ├┤ 5
+				// 2 1  3
+				[
+					middle_bottom,
+					left_bottom,
+					right_bottom,
+					left_middle,
+					right_middle,
+					left_top,
+					right_top,
+					middle_top,
+				]
+            },
+            (Ordering::Equal, Ordering::Less) => {
+                /* player: middle-top */
+                // 2 1  3
+				// 4 ├┤ 5
+				// 6 8  7
+				[
+					middle_top,
+					left_top,
+					right_top,
+					left_middle,
+					right_middle,
+					left_bottom,
+					right_bottom,
+					middle_bottom,
+				]
+            },
+            (Ordering::Equal, Ordering::Equal) => { 
+                /* player: same position */
+                unreachable!();
+            },
+        }
+        .into_iter()
+        .flatten()
+        .collect::<Vec<Coord>>();
+
+        possible_moves
+            .into_iter()
+            .find(|&next_move| board[&next_move] == Tile::Empty)
     }
 }
